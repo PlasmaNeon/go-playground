5c40329105148f28818e4db9b46e1405d829ab42
PlasmaNeon <zedek1ah@outlook.com>
2021-07-26 16:21:18 +0800

    [GeeORM] add schema.

---
+++ GeeORM/schema/schema.go
@@ -0,0 +1,53 @@
+package schema
+
+import (
+	"geeorm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+// Field represents one column information
+type Field struct {
+	Name string //字段名
+	Type string //类型
+	Tag  string //约束条件
+}
+
+// Schema
+type Schema struct {
+	Model     interface{}       //被映射的对象
+	Name      string            //表名
+	Fields    []*Field          //字段
+	FiedNames []string          // 包含所有的字段（列）名
+	fieldMap  map[string]*Field //字段名与Field的映射关系，方便以后直接使用
+}
+
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	schema := &Schema{
+		Model:    dest,
+		Name:     modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modelType.NumField(); i++ {
+		p := modelType.Field(i)                     // Field returns a struct type's i'th field.
+		if !p.Anonymous && ast.IsExported(p.Name) { // p.Anonymous 是否embed; ast.IsExported reports whether name starts with an upper-case letter.
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v, ok := p.Tag.Lookup("geeorm"); ok {
+				field.Tag = v
+			}
+			schema.Fields = append(schema.Fields, field)
+			schema.FiedNames = append(schema.FiedNames, p.Name)
+			schema.fieldMap[p.Name] = field
+		}
+	}
+	return schema
+}
