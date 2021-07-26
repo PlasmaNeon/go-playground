76582ed3c49913248fbae39e9669c85932c96931
PlasmaNeon <zedek1ah@outlook.com>
2021-07-26 18:49:28 +0800

    [GeeORM] add clause.

---
+++ GeeORM/clause/generator.go
@@ -0,0 +1,23 @@
+package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+
+}
+
+func _insert(values ...interface{}) (string, []interface{}) {
+	// INSERT INTO $tableName ($fields)
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("INSERT INTO %s (%v)", tableName, fields), []interface{}{}
+}
