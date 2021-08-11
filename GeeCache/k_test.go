ae5343f05715a0e96b4f09d6dc5c0444ab5e2bb1
PlasmaNeon <zedek1ah@outlook.com>
2021-08-12 01:18:30 +0800

    [GeeCache] update single cache.

---
+++ GeeCache/k_test.go
@@ -0,0 +1,28 @@
+package geecache
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var m sync.Mutex
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	m.Lock()
+	defer m.Unlock()
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+
+}
+
+func TestPrintOnce(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(2 * time.Second)
+}
