ed5c3ac9d8932d7b1c93ba906159ac2fce75447d
PlasmaNeon <zedek1ah@outlook.com>
2021-08-19 17:43:18 -0700

    [GeeCache] distributed peers.

--- GeeCache/geecache/geecache.go
+++ GeeCache/geecache/geecache.go
@@ -17,16 +17,46 @@ func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
 }
 
+// A group is a cache namespace and associated data loaded spread over
 type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
 }
 
 var (
 	mu     sync.RWMutex
 	groups = make(map[string]*Group)
 )
+
+// RegisterPeers 将实现了PeerPicker 接口的 HTTPPool 注入到 Group中
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok { // pick a peer
+			if value, err = g.getFromPeer(peer, key); err == nil { //远程获取
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+	return g.getLocally(key) // 本地获取
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+}
 
 func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 	if getter == nil {
@@ -60,10 +90,6 @@ func (g *Group) Get(key string) (ByteView, error) {
 		return v, nil
 	}
 	return g.load(key)
-}
-
-func (g *Group) load(key string) (value ByteView, err error) {
-	return g.getLocally(key)
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
