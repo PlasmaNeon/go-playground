e48a4f52cf88b8e7a2f8446a83c441c3b71d2cf2
PlasmaNeon <zedek1ah@outlook.com>
2021-08-22 15:45:25 -0700

    [GeeCache] update protobuf.

--- GeeCache/geecache/http.go
+++ GeeCache/geecache/http.go
@@ -3,12 +3,15 @@ package geecache
 import (
 	"fmt"
 	"geecache/consistenthash"
+	pb "geecache/geecachepb"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"sync"
+
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -59,12 +62,13 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	view, err := group.Get(key)
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(view.ByteSlice())
+	w.Write(body)
 }
 
 // Set updates the pool's list of peers.
@@ -96,11 +100,11 @@ type httpGetter struct {
 	baseURL string
 }
 
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(req *pb.Request) (rsp *pb.Response, err error) {
 	u := fmt.Sprintf("%v%v/%v",
 		h.baseURL,
-		url.QueryEscape(group),
-		url.QueryEscape(key),
+		url.QueryEscape(req.Group),
+		url.QueryEscape(req.Key),
 	)
 	res, err := http.Get(u)
 	if err != nil {
@@ -116,7 +120,10 @@ func (h *httpGetter) Get(group string, key string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("reading response body: %v", err)
 	}
-	return bytes, nil
+	if err = proto.Unmarshal(bytes, rsp); err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return rsp, nil
 
 }
 
--- GeeCache/go.mod
+++ GeeCache/go.mod
@@ -1,6 +1,10 @@
 module example
 
-require geecache v0.0.1
+require (
+	geecache v0.0.1
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
 
 go 1.16
 
